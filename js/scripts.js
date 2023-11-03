ecbade56152200472d3f6ca469ca3ae196cf31a5
jeisypaiva1 <128487660+jeisypaiva1@users.noreply.github.com>
2023-11-03 10:53:30 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$('#galeria .container').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+});
